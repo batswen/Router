2bf6ce1227e60594ca5af16546296e0c92ff7484
Swen Rühl <47449782+batswen@users.noreply.github.com>
2019-07-24 22:22:03 +0200

    Added files via upload

---
+++ Router.php
@@ -0,0 +1,184 @@
+<?php
+
+/**
+ * @author      Swen Rühl
+ * @license     MIT public license
+ */
+
+class Router {
+    private $routes = [];
+    private $beforeroutes = [];
+    private $error404 = null;
+    private $controllerpath = null;
+    /**
+     * Constructor
+     * 
+     * @return void
+     */
+    public function __construct($controllerpath) {
+        $this->routes = [];
+        $this->beforeroutes = [];
+        $this->add404(function() { });
+        $this->controllerpath = $controllerpath;
+    }
+
+    /**
+     * REturns base directory
+     * 
+     * @return string basedir
+     */
+    public static function getBaseDir() {
+        return substr($_SERVER['SCRIPT_NAME'], 0, strlen($_SERVER['SCRIPT_NAME']) - 9);
+    }
+
+    /**
+     * @param string $path  The last part of the URL
+     * @return string       The URL
+     */
+    public static function buildURL($path) {
+        return self::getBaseDir() . $path;
+    }
+    
+    /**
+     * Adds a route to the list
+     * 
+     * Add a route and a callback function
+     * 
+     * @param string $method    The method like GET, POST, etc
+     * @param string $route     Part of the URI
+     * @param callable|string $fn      Callback function or 'Method@Controller'
+     * @return void
+     */
+    public function add($method, $route, $fn) {
+        if (strlen($route) > 0 && $route[0] === '/') {
+            $route = substr($route, 1);
+        }
+        foreach (explode('|', $method) as $m) {
+            $this->routes[] = Array('method' => $m, 'fn' => $fn, 'route' => $route);
+        }
+    }
+    
+    /**
+     * Adds a GET route to the list
+     * 
+     * Add a route and a callback function
+     * 
+     * @param string $route     Part of the URI
+     * @param callable|string $fn      Callback function or 'Method@Controller'
+     * @return void
+     */
+    public function addGet($route, $fn) {
+        $this->add('GET', $route, $fn);
+    }
+
+    /**
+     * Adds a before route to the list
+     * 
+     * The before routes are executed first
+     * 
+     * @param string $method    The method like GET, POST, etc
+     * @param string $route     Part of the URI
+     * @param callable|string $fn      Callback function or 'Method@Controller'
+     * @return void
+     */
+    public function addBefore($method, $route, $fn) {
+        if (strlen($route) > 0 && $route[0] === '/') {
+            $route = substr($route, 1);
+        }
+        foreach (explode('|', $method) as $m) {
+            $this->beforeroutes[] = Array('method' => $m, 'fn' => $fn, 'route' => $route);
+        }
+    }
+
+    /**
+     * Adds a before route to the list (GET)
+     * 
+     * The before routes are executed first
+     * 
+     * @param string $route     Part of the URI
+     * @param callable|string $fn      Callback function or 'Method@Controller'
+     * @return void
+     */
+    public function addBeforeGet($route, $fn) {
+        $this->addBefore('GET', $route, $fn);
+    }
+    /**
+     * Adds a 404 route to the list
+     * 
+     * Add a callback
+     * 
+     * @param callable $fn      Callback function
+     * @return void
+     */
+    public function add404($fn) {
+        $this->error404 = $fn;
+    }
+    /**
+     * Starts routing
+     * 
+     * @return int  Number of routes
+     */
+    public function start() {
+        $port = $_SERVER['SERVER_PORT'];
+        $request_uri = $_SERVER['REQUEST_URI'];
+        $request_method = $_SERVER['REQUEST_METHOD'];
+        $query = $_SERVER['QUERY_STRING'];
+        
+        $uri = str_replace(self::getBaseDir(), '', $request_uri);
+
+        $found = 0;
+        if (!empty($this->beforeroutes)) {
+            $this->doRoutes($this->beforeroutes, $uri, $request_method);
+        }
+        if (!empty($this->routes)) {
+            $found = $this->doRoutes($this->routes, $uri, $request_method);
+        }
+
+        # Route not found, show 404 message
+        if ($found === 0 && $this->error404 !== null) {
+            call_user_func($this->error404);
+            $found++;
+        }
+        return $found;
+    }
+    private function doRoutes($routes, $uri, $request_method) {
+        $found = 0;
+        
+        # Test all stored routes
+        foreach ($routes as $route) {
+            $method = $route['method'];
+            $fn = $route['fn'];
+
+            if ($method === $request_method) {
+                if (preg_match('#^' . $route['route'] . '$#', $uri, $matches) === 1) {
+                    # Remove first array element
+                    $matches = array_slice($matches, 1);
+                    # Callback given
+                    if (is_callable($fn)) {
+                        call_user_func_array($fn, $matches);
+                        $found++;
+                    } else if (is_string($fn)) {
+                    # String 'controller@method' given
+                        $splitted = preg_split('/@/', substr($fn, strrpos($fn, '\\')));
+                        $path = substr($fn, 0, strrpos($fn, '\\'));
+                        $controller = $splitted[0];
+                        $method = $splitted[1];
+
+                        $filename = $this->controllerpath . $path . $controller . '.php';
+                        if (file_exists($filename)) {
+                            require_once $filename;
+                            $instance = new $controller();
+                            if ($method !== '' && method_exists($controller, $method)) {
+                                $instance->{$method}($matches);
+                            } else {
+                                $instance->index();
+                            }
+                            $found++;
+                        }
+                    }
+                }
+            }
+        }
+        return $found;
+    }
+}
---
+++ index.php
@@ -0,0 +1,93 @@
+<?php
+session_start();
+
+# Set constant ROOT to this directory
+defined('ROOT') or define('ROOT', dirname(__FILE__) . DIRECTORY_SEPARATOR);
+# Set constant PUBLIC
+defined('PUBLIC_DIR') or define('PUBLIC_DIR', ROOT . 'public' . DIRECTORY_SEPARATOR);
+# Set constant MODELS
+defined('MODELS') or define('MODELS', ROOT . 'app' . DIRECTORY_SEPARATOR . 'models' . DIRECTORY_SEPARATOR);
+# Set constant VIEWS
+defined('VIEWS') or define('VIEWS', ROOT . 'app' . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR);
+# Set constant CONTROLLERS
+defined('CONTROLLERS') or define('CONTROLLERS', ROOT . 'app' . DIRECTORY_SEPARATOR . 'controllers' . DIRECTORY_SEPARATOR);
+
+require ROOT . 'app/inc/Router.php';
+
+$router = new Router(CONTROLLER);
+
+# Matches /
+$router->addGet('', function() { echo 'Homepage!'; });
+
+# /test gives 2 routes
+$router->addGet('test', function() { echo 'Test, first definition!<br>'; });
+$router->addGet('test', function() { echo 'Test, second definition!<br>'; });
+
+# Matches /admin/... before 
+$router->addBefore('GET', '/admin/(.*)', function() use ($router) { 
+    if (!empty($_SESSION['user_level']) && $_SESSION['user_level'] >= 2) {
+        # Logged in as admin
+    } else {
+        header('Location: ' . Router::buildURL('login'));
+        exit();
+    }
+});
+
+# /admin/deleteUser/1673 --> Delete user 1673!
+$router->addGet('admin/deleteUser/(\d+)', function($id) { echo "Delete user $id!"; });
+
+# /admin/renameUser/7/Swen=Test --> Rename user 7 from Test to Swen!
+$router->add('GET', 'admin/renameUser/(\d+)/(\w+)=(\w+)', function($id, $to, $from) { echo "Rename user $id from $from to $to!"; });
+
+# Matches /login | /logout, calls CONTROLLERS.user->login() or logout()
+$router->add('GET', '/login', 'user@login');
+$router->addGet('logout', 'user@logout');
+$router->add('POST', '/loggedin', 'user@loggedin');
+
+# Guess
+$router->add404(function() { exit('404!'); });
+
+# REST Test
+$router->add('POST', 'api/showAllUsers', function() {
+    # Fake authentification
+    $auth = !empty($_SERVER['PHP_AUTH_USER']) && !empty($_SERVER['PHP_AUTH_PW']);
+    if ($auth) {
+        header("Access-Control-Allow-Origin: *");
+        header("Content-Type: application/json; charset=UTF-8");
+
+        # Set up response
+        $users = [];
+        for ($i = 1; $i <= 10; $i++) {
+            $users[] = ['id' => $i, 'name' => 'Name' . $i];
+        }
+        exit(json_encode($users));
+    } else {
+        header('HTTP/1.0 403 Forbidden');
+        exit();
+    }
+});
+
+# Send all data back to the client
+$router->add('POST', 'api/sendback', function() {
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+
+    # Get posted data
+    $data = json_decode(file_get_contents('php://input'), true);
+    exit(json_encode(
+        array('response' => [
+            'send_data' => $data,
+            'auth' => [
+                'user' => !empty($_SERVER['PHP_AUTH_USER']) ? $_SERVER['PHP_AUTH_USER'] : 'unknown',
+                'pw'   => !empty($_SERVER['PHP_AUTH_PW']) ? $_SERVER['PHP_AUTH_PW'] : 'unknown'
+            ]
+        ])
+    ));
+});
+
+# Start the router
+$numroutes = $router->start();
+
+if ($numroutes > 1) {
+    echo "Number of routes: $numroutes";
+}
